38f08e2313a23b1e8eeebb279fd610921f62964b
Honey B <34516348+elizabethjmz@users.noreply.github.com>
2018-08-03 18:38:36 -0500

    Working version of sign in and sign up

--- server.js
+++ server.js
@@ -1,80 +1,59 @@
-var express = require('express');
-var app = express();
-/* For user authentication*/
+var express    = require('express')
+var app        = express()
 var passport   = require('passport')
 var session    = require('express-session')
 var bodyParser = require('body-parser')
-
-
-/* For user authentication*/
-//To import the dot-env module to handle environment variables
-var env = require('dotenv').load();
-
-//Models
-var models = require("./app/models");
-//var config = require("./config/config.json")
-
-var exphbs = require('express-handlebars');
-
-//Routes
-//var authRoute = require('./app/routes/auth.js')(app);
-
-var authRoute = require('./app/routes/auth.js')(app,passport);
-
-//load passport strategies
-require('./app/config/passport/passport.js')(passport, models.user);
-
-//For Handlebars
-app.set('views', './app/views')
-app.engine('hbs', exphbs({
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs');
+var env        = require('dotenv').load()
+var exphbs     = require('express-handlebars')
 
 
 
 //For BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// Static directory
-app.use(express.static("public"));
 
-// For Passport
+
+ // For Passport
 app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
- 
 app.use(passport.initialize());
- 
 app.use(passport.session()); // persistent login sessions
 
 
- 
-app.get('/', function(req, res) {
- 
-    res.send('Welcome to Passport with Sequelize');
- 
+ //For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+
+app.get('/', function(req, res){
+  res.send('Welcome to Intelitica');
 });
- 
- 
+
+
+//Models
+var models = require("./app/models");
+
+
+//Routes
+var authRoute = require('./app/routes/auth.js')(app,passport);
+
+
+//load passport strategies
+require('./app/config/passport/passport.js')(passport,models.user);
+
+
+//Sync Database
+models.sequelize.sync().then(function(){
+console.log("Database is ok")
+
+}).catch(function(err){
+console.log(err,"Something went wrong with the Database Update!")
+});
 
 
 
- //Sync Database
- models.sequelize.sync().then(function() {
-        
-    console.log('Nice! Database looks fine')
-    app.listen(8080, function(err) {
- 
-        if (!err) {
-            console.log("Site is live");
-                
-        }
-        else console.log(err)
-       
-     
-    });
-        
-    }).catch(function(err) {
-        
-        console.log(err, "Something went wrong with the Database Update!")
-        
-    });
+app.listen(8080, function(err){
+    if(!err)
+    console.log("Site is up!"); else console.log(err)
+
+});
